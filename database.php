3ee72b8a02bd1f5729d02f25a7092c9d081c2f8e
Ravlirap <ravli190606@gmail.com>
2025-05-14 08:03:59 +0700

    navbar css

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ $servername = "localhost"; // Change if your server is different
 $username = "root"; // Default username for phpMyAdmin
 $password = ""; // Default password for phpMyAdmin (leave empty if none)
 $database = "proyek1"; // Replace with your database name
-
+ 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
